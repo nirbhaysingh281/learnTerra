c8dc210cb88c3aa12ca46d324b6a3f0f1803a33f
nirbhay singh <nirbhaysingh281@gmail.com>
2021-08-24 14:53:58 +0530

    adding demo2

---
+++ demo2/instance.tf
@@ -0,0 +1,32 @@
+resource "aws_key_pair" "nirbhay_mykey" {
+  key_name   = "nir_mykey"
+  public_key = file(var.PATH_TO_PUBLIC_KEY)
+}
+
+resource "aws_instance" "name" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.nirbhay_mykey.key_name
+
+  provisioner "file" {
+    source      = "script.sh"
+    destination = "/tmp/script.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "sudo sed -i -e 's/\r$//' /tmp/script.sh",
+      "sudo /tmp/script.sh",
+    ]
+
+  }
+
+  connection {
+    host        = coalesce(self.public_ip, self.private_ip)
+    type        = "ssh"
+    user        = var.INSTANCE_USERNAME
+    private_key = file(var.PATH_TO_PRIVATE_KEY)
+  }
+
+}
