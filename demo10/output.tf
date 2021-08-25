75286b1d2158215f1501200bff888a0bd8140895
nirbhay singh <nirbhaysingh281@gmail.com>
2021-08-25 18:13:23 +0530

    added

---
+++ demo10/output.tf
@@ -0,0 +1,4 @@
+output "nir-ecr-url" {
+    value = aws_ecr_repository.nir-ecr.repository_url
+  
+}
---
+++ demo8/vars.tf
@@ -0,0 +1,35 @@
+variable "AWS_ACCESS_KEY" {
+
+}
+
+variable "AWS_SECRET_KEY" {
+
+}
+
+variable "AWS_REGION" {
+  default = "us-west-2"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    "us-west-2" = "ami-08d70e59c07c61a3a"
+  }
+
+}
+
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+
+}
